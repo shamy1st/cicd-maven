16a0c194ca3354b3383f99be4ae53f1cec3eec7f
shamy88 <eng.shamy88@gmail.com>
2020-10-07 20:53:33 +0200

    root access to maven command

--- stages/mvn.sh
+++ stages/mvn.sh
@@ -6,4 +6,4 @@ echo "**************************"
 echo "****** Building jar ******"
 echo "**************************"
 
-docker run --rm -v $WORKSPACE/java-app:/app -v $WORKSPACE/mvn_repo/:/root/.m2/ -w /app maven:3-alpine "$@"
+docker run -u root --rm -v $WORKSPACE/java-app:/app -v $WORKSPACE/mvn_repo/:/root/.m2/ -w /app maven:3-alpine "$@"
