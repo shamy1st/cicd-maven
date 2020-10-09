44e053890d0fe6603a6d34dbfea462378a09616c
shamy88 <eng.shamy88@gmail.com>
2020-10-09 08:57:19 +0200

    add maven command to jenkins container

--- stages/mvn.sh
+++ stages/mvn.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-WORKSPACE=/var/jenkins_home/workspace/pipeline-demo
+APPLICATION=java-app
+JOB=pipeline-demo
+WORKSPACE=/var/jenkins_home/workspace
 
 echo "**************************"
 echo "****** Building jar ******"
 echo "**************************"
 
-sudo docker run --rm -v $WORKSPACE/java-app:/app -v /tmp/root/.m2/:/root/.m2/ -w /app maven:3-alpine "$@"
+#sudo docker run --rm -v $WORKSPACE/java-app:/app -v /tmp/root/.m2/:/root/.m2/ -w /app maven:3-alpine "$@"
+
+cd $WORKSPACE/$JOB/$APPLICATION && "$@"
