9a4af38d8e598be07ac902c2695014b6b3113a81
shamy88 <eng.shamy88@gmail.com>
2020-10-09 11:45:08 +0200

    deploy message

--- stages/deploy.sh
+++ stages/deploy.sh
@@ -3,4 +3,8 @@
 APPLICATION_PATH=$1
 PRODUCTION=$2
 
+echo "***********************"
+echo "****** Deploying ******"
+echo "***********************"
+
 docker cp $APPLICATION_PATH/target/*.jar $PRODUCTION:/app/app.jar
